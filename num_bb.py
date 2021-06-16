e8ae38de5fc2d6ac5d696d1fdaac06bc46e6aca9
SSC94 <68208144+SSC94@users.noreply.github.com>
2021-06-16 22:17:22 +0900

    기능 추가
    1. 잘못된 입력을 받을 시 다시 입력하는 기능에서 문자열 입력시 처리 추가
    2. 정답을 맞춘 후 재시작할지 여부를 묻고 재시작하거나 종료
    3. 안내 텍스트 수정

--- num_bb.py
+++ num_bb.py
@@ -10,11 +10,11 @@ def input_num():
     a = b = c = 0 
     input_lis = [a,b,c]
     for i in range(1,4):
-        input_lis[i-1] = int(input("{}번 숫자 : ".format(i)))
-        while input_lis[i-1] not in range(1,10):
-            input_lis[i-1] = int(input("{}번 숫자를 다시 입력해주세요 : ".format(i)))
-    return input_lis[0], input_lis[1], input_lis[2]
-def startB(a,b,c,lis):
+        input_lis[i-1] = input("{}번 숫자를 입력해주세요. : ".format(i))
+        while input_lis[i-1].isalpha() or int(input_lis[i-1]) not in range(1,10):
+            input_lis[i-1] = input("{}번 숫자를 다시 입력해주세요. : ".format(i))
+    return int(input_lis[0]), int(input_lis[1]), int(input_lis[2])
+def baseball(a,b,c,lis):
     strike = ball = flag = 0
     input_lis = [a,b,c]
     for i in range(3):
@@ -26,17 +26,26 @@ def startB(a,b,c,lis):
             flag = 1
     ball -= strike
     return strike,ball,flag
-answer = create_answer()
-count = 0
-print("시작합니다.")
-while True: 
-    a, b, c = input_num() 
-    strike, ball, flag = startB(a,b,c,answer)
-    count += 1 
-    print("\n입력숫자 : {}{}{}입니다.".format(a,b,c))
-    print("결과 : {}스트라이크 {}볼입니다.".format(strike,ball))
-    print("문제 푼 횟수 : {}번입니다.".format(count),end='\n\n')
-    if flag:
-        print("정답입니다! 걸린 횟수 : {}번입니다.".format(count))
-        print("종료합니다.")
-        break
+def game_start():
+    restart = ''
+    answer = create_answer()
+    count = 0
+    print("시작합니다.\n")
+    while True: 
+        a, b, c = input_num() 
+        strike, ball, flag = baseball(a,b,c,answer)
+        count += 1 
+        print("\n입력 숫자 : {}{}{}입니다.".format(a,b,c))
+        print("결과 : {}스트라이크 {}볼입니다.".format(strike,ball))
+        if flag:
+            print("정답입니다! 총 문제를 푼 횟수 : {}번입니다.\n".format(count))
+            while restart not in ['y','Y','n','N']:
+                restart = input("게임을 다시 시작하시겠습니까? [Y] / [N] \n")
+            if restart == 'y' or restart == 'Y':
+                game_start()
+            else:
+                print("종료합니다.")
+                break
+        else:
+            print("현재 까지 문제를 푼 횟수 : {}번입니다.".format(count),end='\n\n')
+game_start()
